0d00b3dbab7378bc0267826fb783eab2d2cfd408
Rithb898 <105077299+Rithb898@users.noreply.github.com>
2025-04-17 17:41:18 +0530

    fix(webhooks): await clerkClient before updating user metadata to ensure proper execution

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -73,8 +73,10 @@ export async function POST(req: Request) {
 
     const newUser = await createUser(user);
 
+    const client = await clerkClient();
+
     if (newUser) {
-      await (await clerkClient).users.updateUserMetadata(id, {
+      await client.users.updateUserMetadata(id, {
         publicMetadata: {
           userId: newUser._id,
         },
